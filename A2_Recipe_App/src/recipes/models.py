644ed9f1b6dea689f69e78455e7c56f7f8aa4126
Christin-paige <christinpmartin@gmail.com>
2024-05-01 06:22:41 -0400

    and more commits

---
+++ A2_Recipe_App/src/recipes/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.shortcuts import reverse
+
+
+# Create your models here.
+
+
+class Recipe(models.Model):
+    name=models.CharField(max_length=120)
+    ingredients=models.CharField(max_length=250)
+    cooking_time=models.FloatField(help_text='in minutes')
+    pic = models.ImageField(upload_to='recipes/', default='no_picture.jpg')
+
+    def __str__(self):
+        return str(self.name)
+    
+    def get_absolute_url(self):
+       return reverse ('recipes:detail', kwargs={'pk': self.pk})
---
+++ A2_Recipe_App/src/recipes/utils.py
@@ -0,0 +1,77 @@
+from recipes.models import Recipe
+from io import BytesIO
+from collections import Counter
+import base64
+import matplotlib.pyplot as plt
+from django.shortcuts import get_object_or_404
+
+
+
+def get_graph():
+    buffer = BytesIO()#create a BytesIO buffer for the image
+    plt.savefig(buffer, format='png')#create a plot with a bytesIO object as a file-like object. Set format to png
+    buffer.seek(0)#set cursor to the beginning of the stream
+    image_png=buffer.getvalue()#retrieve the content of the file
+    graph=base64.b64encode(image_png)#encode the bytes-like object
+    graph=graph.decode('utf-8')#decode to get the string as output
+    buffer.close()#free up the memory of buffer
+    return graph#return the image/graph
+
+def get_recipename_from_id(val):
+    #this id is used to retrieve name from record
+    recipename=Recipe.objects.get(id=val)
+    return recipename
+
+def get_recipe_ingredient_usage(chart_type):
+    all_ingredients = []
+    for recipe in Recipe.objects.all():
+        ingredients = [ingredient.strip() for ingredient in recipe.ingredients.split(',')]
+        all_ingredients.extend(ingredients)
+      
+    ingredient_counts = Counter(all_ingredients)
+
+     # Extract data for plotting
+    
+
+    #recipe = get_object_or_404(Recipe, pk=recipe_id)
+    chosen_recipe_ingredients = [ingredient.strip() for ingredient in recipe.ingredients.split(',')]#extracts ingredients
+  # Count ingredient occurrences in all recipes (excluding the chosen one)
+    #all_ingredients = Recipe.objects.exclude(pk=recipe_id).values_list('ingredients', flat=True)
+    ingredient_counts = Counter(ingredient. strip() for ingredients in all_ingredients for ingredient in ingredients.split(','))#counts ingredients in particular recipe
+  # Filter ingredient counts based on chosen recipe ingredients
+    filtered_counts = {ingredient: count for ingredient, count in ingredient_counts.items() if ingredient in chosen_recipe_ingredients}
+    ingredients = list(filtered_counts.keys())
+    recipe_counts = list(filtered_counts.values())
+   #select chart_type based on user input from the form
+    if chart_type == '#1':
+       plt.figure(figsize=(8,5))
+       plt.bar(ingredients, recipe_counts)
+       plt.xlabel('Ingredients')
+       plt.ylabel('Number of Recipes')
+       plt.title('Number of Recipes Each Ingredient is In')
+       plt.xticks(rotation=45)
+
+       buffer = BytesIO()#create a BytesIO buffer for the image
+       plt.savefig(buffer, format='png')#create a plot with a bytesIO object as a file-like object. Set format to png
+       plt.close()
+       image_bytes = buffer.getvalue()
+       image_as_string = base64.b64encode(image_bytes).decode('utf-8')
+       return image_as_string
+
+    elif chart_type == '#2':
+       #generate pie chart based
+       plt.figure(figsize=(8,8))
+       plt.pie(recipe_counts, labels=ingredients, autopct='%1.1f%%')
+       plt.title("Percentage of recipes each ingredient is in")
+
+       buffer = BytesIO()#create a BytesIO buffer for the image
+       plt.savefig(buffer, format='png')#create a plot with a bytesIO object as a file-like object. Set format to png
+       plt.close()
+       image_bytes = buffer.getvalue()
+       image_as_string = base64.b64encode(image_bytes).decode('utf-8')
+       return image_as_string
+ 
+    else:
+        return None
+ 
+

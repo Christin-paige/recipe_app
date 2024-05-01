0386d0d182fef440a53c7b504e42aa0f974b1a90
Christin-paige <christinpmartin@gmail.com>
2024-05-01 18:55:49 -0400

    added functioning charts

---
+++ A2_Recipe_App/src/recipes/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from .views import home, CreateRecipeForm, RecipeListView, RecipeDetailView
+from . import views
+
+app_name = 'recipes' 
+
+urlpatterns = [
+   path('', home, name='home'),
+   path('list/', RecipeListView.as_view(), name='list'),
+   path('list/<pk>', RecipeDetailView.as_view(), name='detail'),
+   path('create_recipe', views.create_recipe, name='create_recipe'),
+   path('search_recipe', views.search_recipe, name='search_recipe'),
+   path('search_bar', views.search_bar, name='search_bar')
+  
+]
